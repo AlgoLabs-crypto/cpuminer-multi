530fccafa15fc2ac8522ce19cd361f06b42fe8a6
1NF1N18Y <1nf1n19y@gmail.com>
2024-06-28 22:45:34 +0200

    make x11 my custom algo

--- algo/x11.c
+++ algo/x11.c
@@ -5,117 +5,89 @@
 #include <string.h>
 #include <stdio.h>
 
-#include "sha3/sph_blake.h"
-#include "sha3/sph_bmw.h"
-#include "sha3/sph_groestl.h"
 #include "sha3/sph_jh.h"
 #include "sha3/sph_keccak.h"
-#include "sha3/sph_skein.h"
-#include "sha3/sph_luffa.h"
 #include "sha3/sph_cubehash.h"
-#include "sha3/sph_shavite.h"
-#include "sha3/sph_simd.h"
-#include "sha3/sph_echo.h"
-
+#include "sha3/sph_whirlpool.h"
+#include "sha3/sph_sha2.h"
+#include "sha3/sph_tiger.h"
 
 void x11hash(void *output, const void *input)
 {
-	sph_blake512_context     ctx_blake;
-	sph_bmw512_context       ctx_bmw;
-	sph_groestl512_context   ctx_groestl;
-	sph_skein512_context     ctx_skein;
-	sph_jh512_context        ctx_jh;
-	sph_keccak512_context    ctx_keccak;
+    sph_jh512_context        ctx_jh;
+    sph_keccak512_context    ctx_keccak;
+    sph_cubehash512_context  ctx_cubehash;
+    sph_whirlpool_context    ctx_whirlpool;
+    sph_sha512_context       ctx_sha512;
+    sph_tiger_context        ctx_tiger;
 
-	sph_luffa512_context		ctx_luffa1;
-	sph_cubehash512_context		ctx_cubehash1;
-	sph_shavite512_context		ctx_shavite1;
-	sph_simd512_context		ctx_simd1;
-	sph_echo512_context		ctx_echo1;
+    // These uint512 in the C++ source of the client are backed by an array of uint32
+    uint32_t _ALIGN(64) hashA[16], hashB[16];
 
-	//these uint512 in the c++ source of the client are backed by an array of uint32
-	uint32_t _ALIGN(64) hashA[16], hashB[16];
+    sph_jh512_init(&ctx_jh);
+    sph_jh512(&ctx_jh, input, 80);
+    sph_jh512_close(&ctx_jh, hashA);
 
-	sph_blake512_init(&ctx_blake);
-	sph_blake512 (&ctx_blake, input, 80);
-	sph_blake512_close (&ctx_blake, hashA);
+    sph_keccak512_init(&ctx_keccak);
+    sph_keccak512(&ctx_keccak, hashA, 64);
+    sph_keccak512_close(&ctx_keccak, hashB);
 
-	sph_bmw512_init(&ctx_bmw);
-	sph_bmw512 (&ctx_bmw, hashA, 64);
-	sph_bmw512_close(&ctx_bmw, hashB);
+    sph_cubehash512_init(&ctx_cubehash);
+    sph_cubehash512(&ctx_cubehash, hashB, 64);
+    sph_cubehash512_close(&ctx_cubehash, hashA);
 
-	sph_groestl512_init(&ctx_groestl);
-	sph_groestl512 (&ctx_groestl, hashB, 64);
-	sph_groestl512_close(&ctx_groestl, hashA);
+    sph_whirlpool_init(&ctx_whirlpool);
+    sph_whirlpool(&ctx_whirlpool, hashA, 64);
+    sph_whirlpool_close(&ctx_whirlpool, hashB);
 
-	sph_skein512_init(&ctx_skein);
-	sph_skein512 (&ctx_skein, hashA, 64);
-	sph_skein512_close (&ctx_skein, hashB);
+    sph_sha512_init(&ctx_sha512);
+    sph_sha512(&ctx_sha512, hashB, 64);
+    sph_sha512_close(&ctx_sha512, hashA);
 
-	sph_jh512_init(&ctx_jh);
-	sph_jh512 (&ctx_jh, hashB, 64);
-	sph_jh512_close(&ctx_jh, hashA);
+    sph_tiger_init(&ctx_tiger);
+    sph_tiger(&ctx_tiger, hashA, 64);
+    sph_tiger_close(&ctx_tiger, hashB);
 
-	sph_keccak512_init(&ctx_keccak);
-	sph_keccak512 (&ctx_keccak, hashA, 64);
-	sph_keccak512_close(&ctx_keccak, hashB);
+    sph_sha512_init(&ctx_sha512);
+    sph_sha512(&ctx_sha512, hashB, 64);
+    sph_sha512_close(&ctx_sha512, hashA);
 
-	sph_luffa512_init (&ctx_luffa1);
-	sph_luffa512 (&ctx_luffa1, hashB, 64);
-	sph_luffa512_close (&ctx_luffa1, hashA);
-
-	sph_cubehash512_init (&ctx_cubehash1);
-	sph_cubehash512 (&ctx_cubehash1, hashA, 64);
-	sph_cubehash512_close(&ctx_cubehash1, hashB);
-
-	sph_shavite512_init (&ctx_shavite1);
-	sph_shavite512 (&ctx_shavite1, hashB, 64);
-	sph_shavite512_close(&ctx_shavite1, hashA);
-
-	sph_simd512_init (&ctx_simd1);
-	sph_simd512 (&ctx_simd1, hashA, 64);
-	sph_simd512_close(&ctx_simd1, hashB);
-
-	sph_echo512_init (&ctx_echo1);
-	sph_echo512 (&ctx_echo1, hashB, 64);
-	sph_echo512_close(&ctx_echo1, hashA);
-
-	memcpy(output, hashA, 32);
+    memcpy(output, hashA, 32);
 }
 
 int scanhash_x11(int thr_id, struct work *work, uint32_t max_nonce, uint64_t *hashes_done)
 {
-	uint32_t _ALIGN(128) hash[8];
-	uint32_t _ALIGN(128) endiandata[20];
-	uint32_t *pdata = work->data;
-	uint32_t *ptarget = work->target;
+    uint32_t _ALIGN(128) hash[8];
+    uint32_t _ALIGN(128) endiandata[20];
+    uint32_t *pdata = work->data;
+    uint32_t *ptarget = work->target;
 
-	const uint32_t Htarg = ptarget[7];
-	const uint32_t first_nonce = pdata[19];
-	uint32_t nonce = first_nonce;
-	volatile uint8_t *restart = &(work_restart[thr_id].restart);
+    const uint32_t Htarg = ptarget[7];
+    const uint32_t first_nonce = pdata[19];
+    uint32_t nonce = first_nonce;
+    volatile uint8_t *restart = &(work_restart[thr_id].restart);
 
-	if (opt_benchmark)
-		ptarget[7] = 0x0cff;
+    if (opt_benchmark)
+        ptarget[7] = 0x0cff;
 
-	for (int k=0; k < 19; k++)
-		be32enc(&endiandata[k], pdata[k]);
+    for (int k = 0; k < 19; k++)
+        be32enc(&endiandata[k], pdata[k]);
 
-	do {
-		be32enc(&endiandata[19], nonce);
-		x11hash(hash, endiandata);
+    do {
+        be32enc(&endiandata[19], nonce);
+        x11hash(hash, endiandata);
 
-		if (hash[7] <= Htarg && fulltest(hash, ptarget)) {
-			work_set_target_ratio(work, hash);
-			pdata[19] = nonce;
-			*hashes_done = pdata[19] - first_nonce;
-			return 1;
-		}
-		nonce++;
+        if (hash[7] <= Htarg && fulltest(hash, ptarget)) {
+            work_set_target_ratio(work, hash);
+            pdata[19] = nonce;
+            *hashes_done = pdata[19] - first_nonce;
+            return 1;
+        }
+        nonce++;
 
-	} while (nonce < max_nonce && !(*restart));
+    } while (nonce < max_nonce && !(*restart));
 
-	pdata[19] = nonce;
-	*hashes_done = pdata[19] - first_nonce + 1;
-	return 0;
+    pdata[19] = nonce;
+    *hashes_done = pdata[19] - first_nonce + 1;
+    return 0;
 }
